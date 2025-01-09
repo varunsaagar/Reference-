fb5b886b6d5bafda24c75d03eab89fdb9ee35363
varunsaagar <varunsaagar.s@gmail.com>
2025-01-09 23:36:31 +0530

    Update gemini_agent.py

--- geminiagent/gemini_agent.py
+++ geminiagent/gemini_agent.py
@@ -1,4 +1,22 @@
+(text2sql) [domino@run-677775f203ca6841bc367eca-68v5q geminiagent]$ python3 main.py
+Iteration: 1
+Initial response: content {
+  role: "model"
+  parts {
+    text: "```sql\nSELECT AVG(call_duration_seconds) FROM `vz-it-np-ienv-test-vegsdo-0.vegas_monitoring.icm_summary_fact_exp` WHERE (eccr_dept_nm = \'Technical Support\' OR script_nm LIKE \'%Technical Support%\' OR acd_area_nm LIKE \'%Technical Support%\' OR bus_rule LIKE \'%Technical Support%\') AND DATE(call_end_dt) = DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)\n\n```\n"
+  }
+}
+finish_reason: STOP
+avg_logprobs: -0.0090643376634832011
 
+Extracted SQL Query (before processing): 
+Generated SQL Query: 
+Error executing query: 400 POST https://bigquery.googleapis.com/bigquery/v2/projects/vz-it-np-ienv-test-vegsdo-0/jobs?prettyPrint=false: Required parameter is missing: query
+
+Location: None
+Job ID: c136db70-ea03-4686-b98c-3689657458c6
+
+Final Response: None
 # gemini_agent.py
 import vertexai
 from vertexai.generative_models import (
